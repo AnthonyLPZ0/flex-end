437b5f4b5b59979ade658aeea1b439d7a8307c8b
Anthony <anthonyluisperez3@gmail.com>
2025-08-03 22:52:18 -0500

    JS: se agrego un evento(click) en el navToggle, para que cuando se de el evento se haga visible el nav

--- hambuguer.js
+++ hambuguer.js
@@ -4,3 +4,10 @@ const navToggle = document.querySelector('.nav-toggle');
 //const para la clase nav
 const nav = document.querySelector('.nav');
 
+//indicamos un evento para el nav-toggle, con el evento click
+navToggle.addEventListener("click",() =>{
+
+   //cuando se haga click en el nav-toggle
+   //el nav se mostrara con todos sus links
+   nav.classList.toggle(".nav--visible");
+})
